5ebd84866671ecb90a1f98ff642c355f27a9c20f
vinch <vinchua34@hotmail.com>
2026-06-08 18:48:58 +0800

    Bypass for David

--- src/sceneBuilder.ts
+++ src/sceneBuilder.ts
@@ -2440,7 +2440,7 @@ export class SceneBuilder implements ISceneBuilder {
                 mmdRuntime.setAudioPlayer(audioPlayer);
             }
             if (chosenChar && chosenChar.directory && chosenChar.pmx) {
-                let isSpecialModel = ["Parayaya"].some(name =>
+                let isSpecialModel = ["Parayaya","David"].some(name =>
                     chosenChar.name.includes(name)
                 );
                 if ((chosenChar.name.includes("Mornye")) && (chosenChar.directory.includes("skin"))) {
