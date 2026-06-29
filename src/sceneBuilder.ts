93133452afe5474bc0598fb2d224a00b17c7b604
vinch <vinchua34@hotmail.com>
2026-06-29 18:11:04 +0800

    Remove suisui from exception

--- src/sceneBuilder.ts
+++ src/sceneBuilder.ts
@@ -2440,7 +2440,7 @@ export class SceneBuilder implements ISceneBuilder {
                 mmdRuntime.setAudioPlayer(audioPlayer);
             }
             if (chosenChar && chosenChar.directory && chosenChar.pmx) {
-                let isSpecialModel = ["Parayaya","David", "Adam Smasher", "Suisui"].some(name =>
+                let isSpecialModel = ["Parayaya","David", "Adam Smasher"].some(name =>
                     chosenChar.name.includes(name)
                 );
                 if ((chosenChar.name.includes("Mornye")) && (chosenChar.directory.includes("skin"))) {
